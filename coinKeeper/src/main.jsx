8b748193b7b5d54aaf0f12313a89d7af5b8c1cad
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-05-08 22:45:01 +0500

    Refactor main.jsx to integrate AuthProvider and streamline component structure

--- coinKeeper/src/main.jsx
+++ coinKeeper/src/main.jsx
@@ -1,31 +1,20 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Ваши глобальные стили
-
+import './index.css';
 import { Provider } from 'react-redux';
-import { store } from '../src/redux/store.js'; // Убедитесь, что путь к вашему store правильный
-
-import { BrowserRouter } from 'react-router-dom'; // <--- ИМПОРТИРУЙТЕ ЭТО
+import { store } from '../src/redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter> {/* <--- ОБЕРНИТЕ ВАШЕ ПРИЛОЖЕНИЕ ЗДЕСЬ */}
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
-import { AuthProvider } from './contexts/AuthContext.jsx'; // Импортируйте AuthProvider
-
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <AuthProvider> {/* Оберните App в AuthProvider */}
-                <App />
-            </AuthProvider>
-        </BrowserRouter>
-    </StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
