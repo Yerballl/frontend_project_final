9b6e9b1739c4a3fbe038adede80a702c4fc92ce7
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-05-08 13:36:05 +0500

    Add RegisterPage component with basic structure

---
+++ coinKeeper/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RegisterPage() {
+    return (
+        <div>
+            <h2>Регистрация</h2>
+            {/* Форма регистрации здесь */}
+        </div>
+    );
+}
+
+export default RegisterPage;
